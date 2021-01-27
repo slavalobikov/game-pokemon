d37904126897adec4c03c452936d53158bbbb90b
slava <lobikova12@mail.ru>
2021-01-27 17:01:19 +0300

    homework 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header/Header";
+import Layout from "./components/Layout/Layout";
+import Footer from "./components/Footer/Footer";
+
+
+import bg1 from './components/Layout/bg1.jpg';
+import bg3 from './components/Layout/bg3.jpg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header title={'This is title'} descr={'This is Description!'} />
+        <Layout id={1} title={'title 1'} descr={'descr 1'} urlBg={bg1} />
+        <Layout id={2} title={'title 2'} descr={'descr 2'} colorBg={'whitesmoke'} />
+        <Layout id={3} title={'title 3'} descr={'descr 3'} urlBg={bg3} />
+        <Footer />
+    </>
   );
 }
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import s from './Header.module.css';
+
+const Header = ({title, descr}) => {
+    return (
+        <header className={s.root}>
+            <div className={s.forest} ></div>
+            <div className={s.container}>
+                {!!title && <h1>{title}</h1>}
+                {!!descr && <p>{descr}</p>}
+            </div>
+        </header>
+    );
+};
+
+export default Header;
