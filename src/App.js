d6e217f630749744e334cec480dd110fa0c53154
slava <lobikova12@mail.ru>
2021-01-29 18:12:06 +0300

    homework 3

--- src/App.js
+++ src/App.js
@@ -153,8 +153,7 @@ function App() {
                 on a 3x3 grid.
                 Each player has five cards in a hand and the aim is to capture the opponent's cards by turning them
                 into the player's own color of red or blue.
-              </p>
-              <p>To win, a majority of the total ten cards played (including the one card that is not placed on the
+             To win, a majority of the total ten cards played (including the one card that is not placed on the
                 board) must be of the player's card color. To do this, the player must capture cards by placing a
                 card adjacent to an opponent's card whereupon the 'ranks' of the sides where the two cards touch
                 will be compared. If the rank of the opponent's card is higher than the player's card, the player's
@@ -166,8 +165,8 @@ function App() {
                 <p>In the game two players face off against one another, one side playing as "blue", the other as "red"
                     on a 3x3 grid.
                     Each player has five cards in a hand and the aim is to capture the opponent's cards by turning them
-                    into the player's own color of red or blue.</p>
-                <p>To win, a majority of the total ten cards played (including the one card that is not placed on the
+                    into the player's own color of red or blue.
+                    To win, a majority of the total ten cards played (including the one card that is not placed on the
                     board) must be of the player's card color. To do this, the player must capture cards by placing a
                     card adjacent to an opponent's card whereupon the 'ranks' of the sides where the two cards touch
                     will be compared. If the rank of the opponent's card is higher than the player's card, the player's
@@ -177,7 +176,11 @@ function App() {
             </Layout>
             <Layout id={3} title={'title 3'} descr={'descr 3'} urlBg={bg3}>
                 <div className='flex'>
-                    {POKEMONS.map(item => <PocemonCard {...item} />)}
+                    {POKEMONS.map((item, index) => <PocemonCard key={item.id}
+                                                       values={item.values}
+                                                       type={item.type}
+                                                       name={item.name}
+                                                       img={item.img}  />)}
                 </div>
             </Layout>
             <Footer/>
